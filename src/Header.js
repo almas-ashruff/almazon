b515b6ee123fc600218f19d3402a5bba4098c95d
Almas Khan <almasashruff@gmail.com>
2021-06-25 01:59:04 +0530

    day 1

---
+++ src/Header.js
@@ -0,0 +1,148 @@
+import React from 'react'
+import styled from 'styled-components';
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import RoomIcon from '@material-ui/icons/Room';
+
+
+
+function Header() {
+    return (
+        <Container>
+            <HeaderLogo>
+                <img src={'https://i.imgur.com/7I9Was5.png'}/>
+            </HeaderLogo>
+
+
+            <HeaderOptionAddress>
+                <RoomIcon />
+                <HeaderOption>
+                    <OptionLineOne>
+                        Hello
+                    </OptionLineOne>
+                    <OptionLineTwo>
+                        Select your address
+                    </OptionLineTwo>
+                </HeaderOption>
+                
+            </HeaderOptionAddress>
+
+            <HeaderSearch>
+                <HeaderSearchInput type="text" />
+                <HeaderSearchIconContainer>
+                    <SearchIcon />
+                </HeaderSearchIconContainer>
+            </HeaderSearch> 
+
+            <HeaderNavItems>
+                <HeaderOption>
+                    <OptionLineOne>
+                        Hello, Almas
+                    </OptionLineOne>
+                    <OptionLineTwo>
+                        Account & Lists
+                    </OptionLineTwo>
+                </HeaderOption>
+
+                <HeaderOption>
+                    <OptionLineOne>
+                        Returns
+                    </OptionLineOne>
+                    <OptionLineTwo>
+                        & Orders
+                    </OptionLineTwo>
+                </HeaderOption>
+
+                <HeaderOptionCart>
+                    <ShoppingCartIcon />
+                    <CartCount>
+                        8
+                    </CartCount>
+                </HeaderOptionCart>
+                
+            </HeaderNavItems>    
+        </Container>
+
+    )
+}
+
+export default Header
+
+const Container = styled.div`
+    height: 60px;
+    background-color: #0F1111;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    color: white;
+`
+const HeaderLogo = styled.div`
+    img {
+        width: 100px;
+        margin-left: 10px;
+    }
+`
+
+const HeaderOptionAddress = styled.div`
+    display: flex;
+    align-items: center;
+    padding-left: 10px;
+`
+const OptionLineOne = styled.div`
+
+`
+
+const OptionLineTwo = styled.div`
+    font-weight: 700;
+`
+const HeaderSearch = styled.div`
+    margin-left: 4px;
+    display: flex;
+    flex-grow: 1;
+    height: 40px;
+    border-radius: 4px; 
+    overflow: hidden;
+    background-color: white;
+    :focus-within {
+        box-shadow: 0 0 0 3px #F90;
+    }
+`
+
+const HeaderSearchInput = styled.input`
+    flex-grow: 1;
+    border: 0;
+    :focus {
+         outline: none;
+    }
+`
+
+const HeaderSearchIconContainer = styled.div`
+    background-color: #febd69;
+    width: 45px;
+    color: black;
+    display: flex;
+    justify-content: center;
+    align-items: center;    
+`
+
+const HeaderNavItems = styled.div`
+    display: flex;
+`
+
+
+const HeaderOption = styled.div`
+    padding: 10px 10px 10px 10px;
+`
+
+
+const HeaderOptionCart = styled.div`
+    display: flex;
+    align-items: center;
+    padding-right: 10px;
+
+
+`
+
+const CartCount = styled.div`
+    padding-left: 4px;
+`
